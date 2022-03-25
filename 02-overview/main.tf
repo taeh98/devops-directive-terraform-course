b2c87490ae30219d736dfcdf6f42f29f35d13417
Tom Hardern <tomh@fifthrowtech.com>
2022-03-25 21:17:08 +0000

    moved region to london

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  ami           = "ami-0015a39e4b7c0966f" # Ubuntu 20.04 LTS
   instance_type = "t2.micro"
 }
